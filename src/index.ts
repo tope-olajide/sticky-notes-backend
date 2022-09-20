a8f9112337af24c689126186be19b4b92466af7b
tope-olajide <tope-olajide@users.noreply.github.com>
2022-09-20 13:19:34 +0100

    Create And Connect To MongoDB

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import express from "express";
 import {ApolloServer} from 'apollo-server-express';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import http from 'http';
-
 import { schema } from "./schema";
+import mongoose from 'mongoose';
+require('dotenv').config()
 
 
 async function startApolloServer() {
@@ -13,11 +14,19 @@ async function startApolloServer() {
     schema,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
+
   await server.start();
   server.applyMiddleware({ app, path:'/graphql', cors: false  });
-  const port = process.env.PORT || 4000
+  const port = process.env.PORT || 4000;
   await new Promise<void>(resolve => httpServer.listen({ port }, resolve));
   console.log(`🚀 Server ready at http://localhost:${port+server.graphqlPath}`);
 }
 
 startApolloServer()
+
+async function main() {
+  await mongoose.connect(process.env.DATABASE_URL!);
+  console.log("Connected to mongo atlas successfully...");
+}
+
+main().catch(err => console.log(err));
