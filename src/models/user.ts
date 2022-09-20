eb30274ec1d3950e108264b3f07b684aa35f3a73
tope-olajide <tope-olajide@users.noreply.github.com>
2022-09-20 13:31:03 +0100

    Create The Users Schema

---
+++ src/models/user.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from 'mongoose';
+
+
+interface IUser {
+  fullname: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+
+const userSchema = new Schema<IUser>({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+      },
+      username:{
+        type: String,
+        unique: true,
+        required:true,
+        trim: true
+      },
+      password:{
+        type: String,
+        required: true
+      },
+      fullname:{
+        type: String,
+        required: true,
+        trim: true
+      },
+});
+
+
+const User = model<IUser>('User', userSchema);
+
+export default User;
